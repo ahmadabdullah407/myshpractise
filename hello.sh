0a3fe9899c7c97cf25e3c269fb229df924cf4b77
root <root@ip-172-31-7-167.ap-southeast-1.compute.internal>
2022-01-13 05:31:12 +0000

    hello.sh added

---
+++ hello.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# this is a comment.
+echo "hello world" # this is also a comment.
+
+# system defined variables
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+
+# user defined variables
+name=Mark
+bash: c: command not found
+10val=10
+val=10
+echo The name is $name
+echo the number is $10val #see the problem variable name should not start with a number.
+echo value is $val
